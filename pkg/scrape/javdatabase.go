aa5da503282360d5dd9f390625df6c8edde48599
vt-idiot <81622808+vt-idiot@users.noreply.github.com>
2023-05-15 23:05:22 -0400

    Squashed commit of the following:
    commit a7af3c460721e2f5478537782e93f3397b4495fc
    Author: vt-idiot <81622808+vt-idiot@users.noreply.github.com>
    Date:   Mon May 15 22:43:25 2023 -0400
    
        Update javdatabase.go
    
        Appeasing gofmt...
    
    commit 499749bc725acd57fe20e565ab8390613fff4cfb
    Author: vt-idiot <nyaab00d@gmail.com>
    Date:   Tue May 16 02:36:48 2023 +0000
    
        Add duration to JAV Database scrape

--- pkg/scrape/javdatabase.go
+++ pkg/scrape/javdatabase.go
@@ -1,12 +1,12 @@
 package scrape
 
 import (
-	"strings"
-
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/nleeper/goment"
 	"github.com/xbapps/xbvr/pkg/models"
+	"strconv"
+	"strings"
 )
 
 func ScrapeJavDB(out *[]models.ScrapedScene, queryString string) {
@@ -90,7 +90,12 @@ func ScrapeJavDB(out *[]models.ScrapedScene, queryString string) {
 
 			} else if label == `Content ID:` {
 				contentId = tr.ChildText(`td.tablevalue`)
+
+			} else if label == "Runtime:" {
+				// Duration
+				sc.Duration, _ = strconv.Atoi(strings.Split(tr.ChildText(`td.tablevalue`), " ")[0])
 			}
+
 		})
 
 		// Screenshots
